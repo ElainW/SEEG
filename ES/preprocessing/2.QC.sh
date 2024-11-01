895c0fe40dfb08094df5db0f8c9268938e95e01f
Yilan Wang <yilanwang@g.harvard.edu>
2024-11-01 13:46:15 -0400

    remove sensitive information

--- ES/preprocessing/2.QC.sh
+++ ES/preprocessing/2.QC.sh
@@ -3,10 +3,10 @@
 module load gcc/6.2.0 samtools/0.1.19 bedtools/2.27.1 python/3.7.4
 source ~/yw222PythonFolder/python_bam/bin/activate
 
-INPUT_DIR=/n/groups/walsh/indData/elain/DeepWES4_7/final
-INTERVAL=/n/groups/walsh/indData/elain/DeepWES4_7/S33266340_hs_hg19/S33266340_Regions.nochr.bed
-#BARCODE_LIST=("0863_Ti-G" "0859_Ti-A" "0859_A_" "0863_K" "0873_H" "0864_F" "0861_N_" "0868_R" "0871_J" "0500_B_")
-BARCODE_LIST=("0873_H")
+INPUT_DIR=$ROOT/DeepWES4_7/final
+INTERVAL=S33266340_Regions.nochr.bed
+BARCODE_LIST=("0863_Ti-G" "0859_Ti-A" "0859_A_" "0863_K" "0873_H" "0864_F" "0861_N_" "0868_R" "0871_J" "0500_B_" "0873_H")
+
 for BARCODE_ID in ${BARCODE_LIST[@]}; do
-	sbatch -J $BARCODE_ID -N 1 -c 1 --mem=3G -t 0-3:00 -p short -o logs/${BARCODE_ID}_coverage.log --mail-user=yilanwang@g.harvard.edu --mail-type=FAIL,END calc_avg_cov.sh $INPUT_DIR $BARCODE_ID $INTERVAL
+	sbatch -J $BARCODE_ID -N 1 -c 1 --mem=3G -t 0-3:00 -p short -o logs/${BARCODE_ID}_coverage.log --mail-user=... --mail-type=FAIL,END calc_avg_cov.sh $INPUT_DIR $BARCODE_ID $INTERVAL
 done
