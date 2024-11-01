8de268df39869c66a332e5f56f4a20b395ac939c
Yilan Wang <yilanwang@g.harvard.edu>
2024-11-01 13:53:48 -0400

    add annotations for 2 approaches of downsampling

--- ES/subsample/downsample_Ti_manuscript.sh
+++ ES/subsample/downsample_Ti_manuscript.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+# approach 2
 module load gcc/6.2.0 picard/2.27.5
-# for bed tools
+# for approach 1
 module load bedtools/2.27.1 python/3.7.4
-source ~/yw222PythonFolder/python_bam/bin/activate
 
-bed_int=/n/groups/walsh/indData/elain/DeepWES4_7/S33266340_hs_hg19/S33266340_Regions.nochr.bed
+bed_int=../preprocessing/S33266340_Regions.nochr.bed
 
 DOWNSAMPLE() {
 	while read LINE; do
@@ -17,15 +17,20 @@ DOWNSAMPLE() {
 		echo $outprefix
 		# echo $fraction
 		nt=0-01:00
-		mkdir -p picard/$outprefix
-		sbatch -N 1 -c 4 --mem 3G -J ${outprefix}_downsample -t $nt -p short -o picard/$outprefix/downsampled_picard.log --mail-user=yilanwang@g.harvard.edu --mail-type=FAIL,END --wrap="java -jar $PICARD/picard.jar DownsampleSam I=$in_bam O=$outbam P=$fraction" # picard takes an hour
 
-		# mkdir -p mosdepth_picard/$outprefix
-		# sbatch -N 1 -c 4 --mem 3G -J ${outprefix}_mosdepth_cov -t $nt -p priority -o mosdepth_picard/$outprefix/downsampled_picard_coverage.log --mail-user=yilanwang@g.harvard.edu --mail-type=FAIL,END --wrap="mosdepth -t 4 -b $bed_int mosdepth_picard/$outprefix/subsampled $outbam;\
-		# 	gzip -df mosdepth_picard/$outprefix/subsampled.per-base.bed.gz;\
-		# 	bash calc_avg_cov.sh mosdepth_picard/$outprefix/subsampled $bed_int"
+		# two approaches, either works, choose one
+
+		# approach 1: using mosdepth
+		mkdir -p mosdepth_picard/$outprefix
+		sbatch -N 1 -c 4 --mem 3G -J ${outprefix}_mosdepth_cov -t $nt -p priority -o mosdepth_picard/$outprefix/downsampled_picard_coverage.log --mail-user=yilanwang@g.harvard.edu --mail-type=FAIL,END --wrap="mosdepth -t 4 -b $bed_int mosdepth_picard/$outprefix/subsampled $outbam;\
+			gzip -df mosdepth_picard/$outprefix/subsampled.per-base.bed.gz;\
+			bash calc_avg_cov.sh mosdepth_picard/$outprefix/subsampled $bed_int"
+
+		# approach 2: using picard
+		mkdir -p picard/$outprefix
+		sbatch -N 1 -c 4 --mem 3G -J ${outprefix}_downsample -t $nt -p short -o picard/$outprefix/downsampled_picard.log --mail-user=... --mail-type=FAIL,END --wrap="java -jar $PICARD/picard.jar DownsampleSam I=$in_bam O=$outbam P=$fraction" # picard takes an hour
 		mkdir -p picard_bams
-		sbatch -J ${outprefix}_bedtools_cov -N 1 -c 1 --mem=3G -t 0-1:00 -p priority -o picard_bams/${outprefix}_bedtools_cov.log --mail-user=yilanwang@g.harvard.edu --mail-type=FAIL,END calc_avg_cov_bedtools.sh $outdir $outprefix $bed_int
+		sbatch -J ${outprefix}_bedtools_cov -N 1 -c 1 --mem=3G -t 0-1:00 -p priority -o picard_bams/${outprefix}_bedtools_cov.log --mail-user=... --mail-type=FAIL,END calc_avg_cov_bedtools.sh $outdir $outprefix $bed_int
 	done < subsample_fraction.txt
 }
 
@@ -45,5 +50,5 @@ CHECK_DEPTH() {
 	done < subsample_fraction.txt
 }
 
-# DOWNSAMPLE
+DOWNSAMPLE
 CHECK_DEPTH
