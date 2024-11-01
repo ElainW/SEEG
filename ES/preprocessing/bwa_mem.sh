98df57da044504ad2bd2068dbd70b2990fc72a0f
Yilan Wang <yilanwang@g.harvard.edu>
2024-11-01 13:32:41 -0400

    remove sensitive information

--- ES/preprocessing/bwa_mem.sh
+++ ES/preprocessing/bwa_mem.sh
@@ -5,4 +5,4 @@ FQ1=$2
 FQ2=$3
 OUTDIR=$4
 
-bwa mem -t ${THREAD_NUM} -C -M -R '@RG\tID:${BARCODE_ID}\tSM:human\tPL:illumina' /home/yh174/reference/human_v37_contig_hg19_hs37d5/human_v37_contig_hg19_hs37d5.fasta $FQ1 $FQ2 | samtools view -Sb - > $OUTDIR/${BARCODE_ID}.bam
+bwa mem -t ${THREAD_NUM} -C -M -R '@RG\tID:${BARCODE_ID}\tSM:human\tPL:illumina' $REFERENCE_DIR/human_v37_contig_hg19_hs37d5/human_v37_contig_hg19_hs37d5.fasta $FQ1 $FQ2 | samtools view -Sb - > $OUTDIR/${BARCODE_ID}.bam
